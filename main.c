1ead86fdc55cb7ee17d98bcbecbff9518a3e7ba0
tkhawari <taimurhaider111@hotmail.com>
2015-01-20 19:14:48 +1100

    Update main.c

--- main.c
+++ main.c
@@ -5,13 +5,14 @@ int main(void)
 {
   int counter;
   int i;
+  int is_prime=0;
   
   for (counter=2; counter <=100; counter++)
   {
     for (i=2; i<=counter-1; i++)
     {
       if (i %counter != 0)
-      
+      return is_prime;
     }
     
     printf("%i,", counter);
