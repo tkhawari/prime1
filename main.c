ff6074c07cb288862223d647c1044891e509ae57
tkhawari <taimurhaider111@hotmail.com>
2015-01-20 19:29:24 +1100

    Update main.c

--- main.c
+++ main.c
@@ -5,10 +5,11 @@ int main(void)
 {
   int counter;
   int i;
-  int is_prime=1;
+  int is_prime;
   
   for (counter=2; counter <=100; counter++)
   {
+    is_prime=1;
     for (i=2; i<=counter-1; i++)
     {
       if (i %counter != 0)
