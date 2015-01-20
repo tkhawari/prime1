cea15ff8786799bf93252ee88d8c35cfbaad2695
tkhawari <taimurhaider111@hotmail.com>
2015-01-20 18:50:16 +1100

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,15 @@
 int main(void)
 {
   int counter;
+  int i;
   
   for (counter=2; counter <=100; counter++)
   {
+    for (i=2; i<=counter-1; i++)
+    {
+      if (i %counter != 0)
+      printf("%i", counter)
+    }
     
     printf("%i,", counter);
   }
