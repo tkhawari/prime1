3013ead4675f86211cdc7c99b57c816fd7748d42
tkhawari <taimurhaider111@hotmail.com>
2015-01-20 20:52:17 +1100

    Update prime3.c

--- prime3.c
+++ prime3.c
@@ -1,3 +1,6 @@
+/* this program is similar to prime2.c execpt for higher efficiency, to check if number n is prime, 
+you do not need to check divisors from 2 to n-1 . checking divisors from 2 to root square of n is enough. 
+I speed matters when dealing with huge numers! */
 int main(void)
 
 {
